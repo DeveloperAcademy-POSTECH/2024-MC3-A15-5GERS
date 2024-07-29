698daec5a3b3ec7c37c87e4eab9f23c9daf5766e
Jeong Dong <eejxdong99@gmail.com>
2024-07-29 20:39:53 +0900

    feat: Notification, LiveActivity Manager 정의 및 구현

--- 5GERS/Manager/LiveActivityManager.swift
+++ 5GERS/Manager/LiveActivityManager.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import ActivityKit
+
+final class LiveActivityManager {
+    static let shared = LiveActivityManager()
+    private init() {}
+    
+    func startActivity(_ time: Date) throws {
+        let attributes = LiveActivityWidgetAttributes(time: time)
+        let state = LiveActivityWidgetAttributes.ContentState()
+        
+        do {
+            let _ = try Activity.request(
+                attributes: attributes,
+                content: .init(
+                    state: state,
+                    staleDate: nil
+                )
+            )
+        } catch {
+            throw error
+        }
+    }
+    
+    func isActivateActivity() -> Bool {
+        return !Activity<LiveActivityWidgetAttributes>.activities.isEmpty
+    }
+    
+    func endActivity() async {
+        for activity in Activity<LiveActivityWidgetAttributes>.activities {
+            await activity.end(nil, dismissalPolicy: .immediate)
+        }
+    }
+}
--- 5GERS/Manager/NotificationManager.swift
+++ 5GERS/Manager/NotificationManager.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import UserNotifications
+
+final class NotificationManager {
+    static let shared = NotificationManager()
+    private init() {}
+    
+    func requestPermission() {
+        UNUserNotificationCenter
+            .current()
+            .requestAuthorization(
+                options: [.alert, .sound]
+            ) { granted, error in
+                if granted {
+                    // 권한이 허용된 경우
+                    print("Notification permission granted.")
+                } else {
+                    // 권한이 거부된 경우
+                    print("Notification permission denied.")
+                }
+            }
+    }
+    
+    func scheduleAlarmNotification(at date: Date) {
+        let content = UNMutableNotificationContent()
+        content.title = "Custom Alarm"
+        content.body = "It's time for your custom alarm!"
+        content.sound = UNNotificationSound.default
+        
+        let triggerDate = Calendar.current.dateComponents([.hour, .minute], from: date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: true)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error adding notification: \(error.localizedDescription)")
+            }
+        }
+        
+    }
+}
