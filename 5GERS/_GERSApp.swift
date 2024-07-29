fb852b890e2ac1432a8e2d149571c559627a4917
Jeong Dong <eejxdong99@gmail.com>
2024-07-29 19:40:47 +0900

    chore: 프로젝트 생성

---
+++ 5GERS/_GERSApp.swift
@@ -0,0 +1,17 @@
+//
+//  _GERSApp.swift
+//  5GERS
+//
+//  Created by 이정동 on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct _GERSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
